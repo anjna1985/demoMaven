dc4cb198ac321b72a3435dd401323d7aea5eaa26
14012 <anjna85@gmail.com>
2020-10-03 11:23:59 -0400

    Commiting

---
+++ src/MavenDemoo.java
@@ -0,0 +1,14 @@
+
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class MavenDemoo {
+	
+	@Test
+	public void display() {
+				System.out.println("Hello Dear");
+	
+	}
+
+}
